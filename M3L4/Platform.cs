fa4a325f2f6c29b47062ecb74e321c4c27579251
kritoy228 <119926632+kritoy228@users.noreply.github.com>
2023-01-10 18:56:10 +1000

    модуль 3 урок 5

---
+++ M3L4/Platform.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float speed;
+    public Vector3 direction;
+    public bool isActive;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+        }
+    }  
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.tag == "PlatformStop")
+        {
+            direction *= -1;
+        }
+        if(other.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            isActive = false;
+        }
+    }
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+
+
+}
---
+++ M3L4/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip damageSound; 
+    //Максимальное здоровье игрока
+    private int health = 10;
+
+    //Число собранных монет
+    private int coins;
+
+    //Префаб огненнего шара и параметр Transform точки атаки
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+
+
+    //Метод, понижающий здоровье игрока
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        print("Здоровье игрока: " + health);
+        if (health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+
+//Метод, увеличивающий число монеток
+public void CollectCoins()
+    {
+        coins++;
+        print("Собранные монетки: " + coins);
+    }
+
+
+    void Update()
+    {
+
+        //Если игрок кликает левой кнопкой мыши, то создаётся огненный шар
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, attackPoint.position, attackPoint.rotation);
+        }
+
+    }
+}
